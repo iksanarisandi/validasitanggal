2e3e89272f249e1ac6260563f8d1db748e5f75c7
iksanarisandi <33199507+iksanarisandi@users.noreply.github.com>
2025-03-10 13:48:49 +0800

    Update validasitanggal1517.js

--- validasitanggal1517.js
+++ validasitanggal1517.js
@@ -1,5 +1,9 @@
-// Validasi jika tanggal adalah 15, 16, atau 17 → stop eksekusi
-const today = new Date().getDate();
-if (today >= 10 && today <= 17) {
-    throw new Error(`Eksekusi dihentikan karena hari ini adalah tanggal ${today}`);
+const today = new Date();
+const tanggal = today.getDate();
+
+if (tanggal >= 9 && tanggal <= 17) {
+    console.log("Dihentikan karena masuk tanggal 15-17");
+    throw new Error("Workflow dihentikan karena tanggal termasuk 15-17");
+} else {
+    console.log("Lanjutkan workflow");
 }
