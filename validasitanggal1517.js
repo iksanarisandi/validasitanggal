8725bf5c61c9b8f331b5ef57b65ba5eba8d1cc06
iksanarisandi <33199507+iksanarisandi@users.noreply.github.com>
2025-03-10 13:45:42 +0800

    Update validasitanggal1517.js

--- validasitanggal1517.js
+++ validasitanggal1517.js
@@ -1,5 +1,5 @@
 // Validasi jika tanggal adalah 15, 16, atau 17 → stop eksekusi
 const today = new Date().getDate();
-if (today >= 15 && today <= 17) {
+if (today >= 10 && today <= 17) {
     throw new Error(`Eksekusi dihentikan karena hari ini adalah tanggal ${today}`);
 }
